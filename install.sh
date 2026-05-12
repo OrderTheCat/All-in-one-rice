55dd47d01f8e4da45d8524678bd4b77894d2702e
OrderTheCat <nordvpn.gathering491@aleeas.com>
2026-05-12 13:17:59 +0530

    added catppuccin icons

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ DIR1=~/All-in-one-rice/fastfetch
 DIR2=~/All-in-one-rice/kitty
 DIR3=~/All-in-one-rice/'Mouse Cursors'/'Bibata Modern Ice [Cursor]'
 DIR4=~/All-in-one-rice/topgrade.toml
-DIR5=~/All-in-one-rice/'Mouse Cursors'/'Adwaita [Cursor]'
+DIR5=~/All-in-one-rice/'Mouse Cursors'/'Adwaita [Cursor]'"
+DIR6=~/All-in-one-rice/icons/My-Catppuccin
 
 # Destination directories
 DEST1=~/.config
@@ -13,6 +14,7 @@ DEST2=~/.config
 DEST3=~/.icons
 DEST4=~/.config
 DEST5=~/.icons
+DEST6=~/.local/share/icons/
 
 # Move directories
 mv "$DIR1" "$DEST1"
